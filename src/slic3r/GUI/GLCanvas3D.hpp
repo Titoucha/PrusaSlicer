efa29b81e7077e9e9c2deb595f1d82d3b0c8fa85
tamasmeszaros <meszaros.q@gmail.com>
2020-11-18 00:06:34 +0100

    Right mouse reaction to arrange button
    Working arrange settings popup

--- src/slic3r/GUI/GLCanvas3D.hpp
+++ src/slic3r/GUI/GLCanvas3D.hpp
@@ -450,6 +450,13 @@ public:
         Cross
     };
 
+    struct ArrangeSettings
+    {
+        float distance         = 6.;
+        float accuracy         = 0.65f;
+        bool  enable_rotation  = false;
+    };
+
 private:
     wxGLCanvas* m_canvas;
     wxGLContext* m_context;
@@ -532,6 +539,8 @@ private:
     mutable Tooltip m_tooltip;
     mutable bool m_tooltip_enabled{ true };
     Slope m_slope;
+
+    ArrangeSettings m_arrange_settings;
 
 public:
     explicit GLCanvas3D(wxGLCanvas* canvas);
@@ -768,6 +777,8 @@ public:
     void use_slope(bool use) { m_slope.use(use); }
     void set_slope_normal_angle(float angle_in_deg) { m_slope.set_normal_angle(angle_in_deg); }
 
+    const ArrangeSettings& get_arrange_settings() const { return m_arrange_settings; }
+
 private:
     bool _is_shown_on_screen() const;
 
@@ -819,6 +830,7 @@ private:
     void _render_selection_sidebar_hints() const;
     bool _render_undo_redo_stack(const bool is_undo, float pos_x) const;
     bool _render_search_list(float pos_x) const;
+    bool _render_arrange_popup();
     void _render_thumbnail_internal(ThumbnailData& thumbnail_data, bool printable_only, bool parts_only, bool show_bed, bool transparent_background) const;
     // render thumbnail using an off-screen framebuffer
     void _render_thumbnail_framebuffer(ThumbnailData& thumbnail_data, unsigned int w, unsigned int h, bool printable_only, bool parts_only, bool show_bed, bool transparent_background) const;
